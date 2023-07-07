ce28eede63b6f7c0a9e333563d191a6e7551a44e
Ed Oz <orozcorp@gmail.com>
2023-07-07 15:15:10 -0600

    testing

---
+++ src/app/Admin/Resume/AddToResume.js
@@ -0,0 +1,9 @@
+import Modal from "@/components/smallComponents/Modal";
+
+export default function AddToResume({ display, setDisplay }) {
+  return (
+    <Modal display={display} setDisplay={setDisplay} title="Add to resume">
+      AddToResume
+    </Modal>
+  );
+}
--- src/components/smallComponents/Modal.js
+++ src/components/smallComponents/Modal.js
@@ -1,15 +1,13 @@
 "use client";
-import { useRouter } from "next/navigation";
-export default function Modal({ title, children }) {
-  const router = useRouter();
+export default function Modal({ title, children, display, setDisplay }) {
   return (
     <div
-      id="defaultModal"
       tabindex="-1"
       aria-hidden="true"
-      className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      className="relative z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      style={{ display: display }}
     >
-      <div className="relative w-full max-w-2xl max-h-full">
+      <div className="fixed  w-full max-w-2xl max-h-full drop-shadow-2xl shadow-2xl ">
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
           <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
             <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
@@ -18,7 +16,7 @@ export default function Modal({ title, children }) {
             <button
               type="button"
               className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              onClick={() => router.back()}
+              onClick={() => setDisplay("none")}
             >
               <svg
                 className="w-3 h-3"
@@ -29,17 +27,16 @@ export default function Modal({ title, children }) {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
                 />
               </svg>
               <span className="sr-only">Close modal</span>
             </button>
           </div>
-
-          {children}
+          <div class="p-6 space-y-6">{children}</div>
         </div>
       </div>
     </div>
