0fd66051fdcdd673bb3d8c29bf05c0bb1d213270
Ed Oz <orozcorp@gmail.com>
2023-07-12 11:51:30 -0600

    updated

--- src/app/(Home)/Hero/page.js
+++ src/app/(Home)/Hero/page.js
@@ -1,9 +1,5 @@
-import React from "react";
-
 export default function Hero() {
   return (
-    <div className="flex flex-row flex-wrap border w-full h-96 shadow-lg rounded-3xl mt-8">
-      Hero
-    </div>
+    <div className="flex flex-row flex-wrap border w-full h-96 shadow-lg rounded-3xl mt-8"></div>
   );
 }
