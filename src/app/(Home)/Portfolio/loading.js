69086bc26f412b4759c9ba54142173e9a3db2e3c
Ed Oz <orozcorp@gmail.com>
2023-08-10 15:11:59 -0600

    TEsting server

--- src/app/(Home)/Portfolio/loading.js
+++
@@ -1,9 +0,0 @@
-import Spinner from "@/components/smallComponents/Spinner";
-
-export default function loading() {
-  return (
-    <div className="flex flex-col flex-nowrap h-screen w-screen justify-center items-center">
-      <Spinner color="sky-800" />
-    </div>
-  );
-}
--- src/app/Admin/Resume/[id]/loading.js
+++
@@ -1,8 +0,0 @@
-import Spinner from "@/components/smallComponents/Spinner";
-export default function loading() {
-  return (
-    <div className="flex flex-column flex-nowrap w-full h-screen items-center justify-center">
-      <Spinner color="sky-800" />
-    </div>
-  );
-}
