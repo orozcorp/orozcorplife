ba7f44efedcff0eab6157392732f3cff88121453
Ed Oz <orozcorp@gmail.com>
2023-09-20 06:05:49 -0600

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,18 @@
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
 // const Hero = dynamic(() => import("./(Home)/Hero/page"));
-const Articles = dynamic(() => import("./(Home)/Articles/page"));
-const Portfolio = dynamic(() => import("./(Home)/Portfolio/page"));
-const Resume = dynamic(() => import("./(Home)/Resume/page"));
-const Contact = dynamic(() => import("./(Home)/Contact/page"));
+import Articles from "./(Home)/Articles/page";
+import Portfolio from "./(Home)/Portfolio/page";
+import Resume from "./(Home)/Resume/page";
+import Contact from "./(Home)/Contact/page";
+
 export default function Home() {
   return (
     <main className="flex flex-col flex-nowrap w-full justify-center items-center">
       {/* <Hero /> */}
       <div className="flex flex-row flex-wrap p-4 w-full md:w-3/4 items-center justify-center">
         <div className="flex flex-col flex-nowrap w-full justify-start">
-          <Suspense fallback={<p>Loading articles...</p>}>
-            <Articles />
-          </Suspense>
-          <Suspense fallback={<p>Loading portfolio...</p>}>
-            <Portfolio />
-          </Suspense>
-          <Suspense fallback={<p>Loading resume...</p>}>
-            <Resume />
-          </Suspense>
+          <Articles />
+          <Portfolio />
+          <Resume />
           <Contact />
         </div>
       </div>
